42c4af0bf3f7f5934c3321f97c5037b2909f4bd2
radil <adil.r@northeastern.edu>
2025-01-13 14:04:37 -0500

    - fixed file counting bug - added brackets around create,drop, and truncate section of scripts

--- model/model.py
+++ model/model.py
@@ -4,6 +4,8 @@ from model.exceptions_and_constants import *
 '''
 This class acts as the model for Ramzi's Delimited File SQL Writing Tool program.
 '''
+
+
 class script_writer_model():
     def __init__(self):
         self.file_number_counter = 1
@@ -17,7 +19,7 @@ class script_writer_model():
         self.row_terminator = None
         self.target_files = []
 
-        #used in loop to catch errors
+        # used in loop to catch errors
         self.current_create_script_section = None
         self.current_import_script_section = None
 
@@ -28,7 +30,6 @@ class script_writer_model():
         if not hasattr(cls, 'instance'):
             cls.instance = super(script_writer_model, cls).__new__(cls)
         return cls.instance
-
 
     def reset_file_model_parameters(self):
         '''
@@ -50,10 +51,10 @@ class script_writer_model():
 
     def is_delimiter_empty(self) -> None:
         '''
-        Used to check if user entry for delimiter has been set.
-        raises an error if not.
-        :return: @None
-        '''
+		Used to check if user entry for delimiter has been set.
+		raises an error if not.
+		:return: @None
+		'''
         if self.delimiter == '':
             raise CustomExceptionMissingEntry('DELIMITER')
         else:
@@ -61,10 +62,10 @@ class script_writer_model():
 
     def is_target_dir_empty(self) -> None:
         '''
-        Used to check if user entry for target directory has been set.
-        raises an error if not.
-        :return: @None
-        '''
+		Used to check if user entry for target directory has been set.
+		raises an error if not.
+		:return: @None
+		'''
         if self.target_dir_path == '':
             raise CustomExceptionMissingEntry('Target Directory')
         else:
@@ -72,10 +73,10 @@ class script_writer_model():
 
     def is_export_dir_empty(self) -> None:
         '''
-        Used to check if user entry for export directory has been set.
-        raises an error if not.
-        :return: @None
-        '''
+		Used to check if user entry for export directory has been set.
+		raises an error if not.
+		:return: @None
+		'''
         if self.export_directory_path == '':
             raise CustomExceptionMissingEntry('File Export Directory')
         else:
@@ -121,23 +122,23 @@ class script_writer_model():
 
     def check_user_defined_attributes(self) -> None:
         '''
-        Checks all user defined entries. Raises an error if any are
-        encountered.
-        :return: @None
-        '''
+		Checks all user defined entries. Raises an error if any are
+		encountered.
+		:return: @None
+		'''
         self.is_delimiter_empty()
         self.is_target_dir_empty()
         self.is_export_dir_empty()
 
-    def get_all_filenames(self,target_directory: str) -> list:
+    def get_all_filenames(self, target_directory: str) -> list:
         '''
         Get all filenames from a specific directory
         :param target_directory: @str filepath to target directory
         :return: @list list of files from the target directory
         '''
         return os.listdir(target_directory)
-    
-    def make_create_tables_fields(self,filepath_in: str, delimiter_in: str) -> str:
+
+    def make_create_tables_fields(self, filepath_in: str, delimiter_in: str) -> str:
         '''
         Helper func to make_create_tables_scripts
         :param filepath_in: @str path to the file to look at
@@ -145,24 +146,24 @@ class script_writer_model():
         :return: @str the fields section of a create table specific to the filepath_in
         '''
         temp = []
-        empty_field_counter = 1 #used to keep count of how many empty fields there are if any
+        empty_field_counter = 1  # used to keep count of how many empty fields there are if any
         file_name = filepath_in.split('\\').pop()
 
         try:
-            #read the header from the file and store the fields
+            # read the header from the file and store the fields
             with open(filepath_in, 'r') as filereader:
                 headers_line = filereader.readline()
                 all_fields_unformatted = headers_line.split(delimiter_in)
 
                 for i in range(len(all_fields_unformatted)):
-                    field = all_fields_unformatted[i].replace(' ', '_') #replacing space char with underscore
-                    field = field.replace('\n','') #for last field remove newline char
-                    field = field.replace('\"','') #remove quotes in field names
+                    field = all_fields_unformatted[i].replace(' ', '_')  # replacing space char with underscore
+                    field = field.replace('\n', '')  # for last field remove newline char
+                    field = field.replace('\"', '')  # remove quotes in field names
 
                     for char in NON_VALID_CHARS_FIELD_NAME:
-                        field = field.replace(char,'') #replace any non_valid field chars
+                        field = field.replace(char, '')  # replace any non_valid field chars
 
-                    if field in WHITE_SPACE_CHAR_LIST or field == '': #column'fields with no name
+                    if field in WHITE_SPACE_CHAR_LIST or field == '':  # column'fields with no name
                         field = f'NO_FIELD_NAME_{str(empty_field_counter)}'
                         empty_field_counter += 1
 
@@ -172,10 +173,10 @@ class script_writer_model():
                         temp.append(f'\t\t[{field}] varchar(max),\n')
         except UnicodeDecodeError:
             raise CustomExceptionInvalidFile(file_name)
-    
+
         return ''.join(temp)
-    
-    def make_create_tables_script(self,filepath_in: str,delimiter_in: str) -> str:
+
+    def make_create_tables_script(self, filepath_in: str, delimiter_in: str) -> str:
         """
         Stores the sections of the create table script that will eventually
         get written into one create table file
@@ -183,7 +184,7 @@ class script_writer_model():
         :return: @None
         """
         insert_list = []
-    
+
         file_name = filepath_in.split('\\').pop()
 
         # get file name no extension. Did it this way in case extension uses multiple periods
@@ -191,25 +192,25 @@ class script_writer_model():
         file_name_no_extension = file_name_list.pop(0)
 
         table_name = file_name_no_extension
-        table_name = table_name.replace(' ','_') #remove space char in table name so don't have to use brackets when calling table name
-    
+        table_name = table_name.replace(' ',
+                                        '_')  # remove space char in table name so don't have to use brackets when calling table name
+
         for invalid_char in NON_VALID_CHARS_TABLE_NAME:
-            table_name = table_name.replace(invalid_char,'')
-    
-        if table_name.upper() in RESERVED_KEYWORDS: #raise error if table name is a keyword in sql
+            table_name = table_name.replace(invalid_char, '')
+
+        if table_name.upper() in RESERVED_KEYWORDS:  # raise error if table name is a keyword in sql
             raise CustomExceptionInvalidTableName(table_name, file_name)
-    
-        #generate the create script per file
-        ct_drop = f'IF OBJECT_ID (\'{table_name}\', \'U\') IS NOT NULL\n\tDROP TABLE dbo.{table_name};\n\n'
-        fields_as_string = self.make_create_tables_fields(filepath_in,delimiter_in)
-        ct_create = f'CREATE TABLE {table_name}\n\t(\n' + fields_as_string + '\t)\n\n' + '--' + ('='*50) + '\n'
+
+        # generate the create script per file
+        ct_drop = f'IF OBJECT_ID (\'{table_name}\', \'U\') IS NOT NULL\n\tDROP TABLE [dbo].[{table_name}];\n\n'
+        fields_as_string = self.make_create_tables_fields(filepath_in, delimiter_in)
+        ct_create = f'CREATE TABLE [{table_name}]\n\t(\n' + fields_as_string + '\t)\n\n' + '--' + ('=' * 50) + '\n'
         insert_list.append(ct_drop)
         insert_list.append(ct_create)
 
         return ''.join(insert_list)
 
-    
-    def wrap_in_try_catch(self,input: str, table_name_in: str,filename: str) -> str:
+    def wrap_in_try_catch(self, input: str, table_name_in: str, filename: str) -> str:
         """
         Used in the generation of the insert scripts by wrapping them in a try catch
         :param input: @str the insert that goes in between the try catch
@@ -226,7 +227,7 @@ class script_writer_model():
         fifth = f'\n\tPRINT @err_msg{self.file_number_counter} \n' + 'END CATCH\'\n'
         sixth = f'exec(@command{self.file_number_counter})'
         return zero + first + second + third + fourth + fifth + sixth
-    
+
     def make_insert_script(self, filepath_in: str, delimiter_in: str) -> str:
         """
         Generates the insert script sections
@@ -236,31 +237,31 @@ class script_writer_model():
         """
         file_name = filepath_in.split('\\').pop()
 
-        #get file name no extension. Did it this way in case extension uses multiple periods
+        # get file name no extension. Did it this way in case extension uses multiple periods
         file_name_list = file_name.split('.')
         file_name_no_extension = file_name_list.pop(0)
 
-        #get extension
+        # get extension
         extension = '.'.join(file_name_no_extension)
 
-        table_name = file_name_no_extension # this is full name including the dot
+        table_name = file_name_no_extension  # this is full name including the dot
         table_name = table_name.replace(' ',
                                         '_')  # remove space char in table name so don't have to use brackets
-                                                    # when calling table name
-    
-        for invalid_char in NON_VALID_CHARS_TABLE_NAME: # Remove invalid characters for table names
+        # when calling table name
+
+        for invalid_char in NON_VALID_CHARS_TABLE_NAME:  # Remove invalid characters for table names
             table_name = table_name.replace(invalid_char, '')
-    
-        if table_name.upper() in RESERVED_KEYWORDS: #raise error if table name is a keyword in sql
+
+        if table_name.upper() in RESERVED_KEYWORDS:  # raise error if table name is a keyword in sql
             raise CustomExceptionInvalidTableName(table_name, file_name)
-    
+
         if delimiter_in in REP_DICT.keys():
-            delim = REP_DICT[delimiter_in] #replace delimiter with delimiter to write in script if needed
+            delim = REP_DICT[delimiter_in]  # replace delimiter with delimiter to write in script if needed
         else:
             delim = delimiter_in
-    
-        trunc_cmd = f'\nIF OBJECT_ID (\'{table_name}\', \'U\') IS NOT NULL\n\tTRUNCATE TABLE dbo.{table_name};\n\n'
-    
+
+        trunc_cmd = f'\nIF OBJECT_ID (\'{table_name}\', \'U\') IS NOT NULL\n\tTRUNCATE TABLE [dbo].[{table_name}];\n\n'
+
         self.whole_bulk_insert_content_list.append(trunc_cmd)
 
         file_ins = 'file' + str(self.file_number_counter)
@@ -271,21 +272,20 @@ class script_writer_model():
         s2 = f'FROM \'\'\' + @{file_ins} + \'\'\' WITH\n'
         s3 = f'\t(\n\tFIELDTERMINATOR = \'\'{delim}\'\',\n\tROWTERMINATOR = \'\'{self.row_terminator}\'\'\n\t)'
 
-    
         # files will always have headers so no need for if statement
         s3 = s3.lstrip('\t(')
         s3 = f'\t(\n\tFIRSTROW = 2,' + s3
-    
-        temp = [s1,s2,s3]
-    
+
+        temp = [s1, s2, s3]
+
         if extension.lower() == 'csv':
             s3 = temp.pop()
             s3 = s3.rstrip('\n\t)')
             s3 = s3 + ",\n\tFORMAT = ''CSV''\n\t)"
             temp.append(s3)
-    
+
         t_str = ''.join(temp)
-        t_str = self.wrap_in_try_catch(t_str,table_name,filepath_in.split('\\').pop())
+        t_str = self.wrap_in_try_catch(t_str, table_name, filepath_in.split('\\').pop())
         t_str += '\n--' + ('=' * 50)
 
         return t_str
